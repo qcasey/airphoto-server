d2a6c275de079f11c78ed701db32e4b4cfd621b9
Quinn Casey <qcasey1@gmail.com>
2020-06-29 17:03:20 -0700

    dont use absolute dirs

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./airphoto --firebase --db /Users/quinncasey/Library/MediaStream/albumshare/1354123432+6916374443373062918/Model.sqlite --assets "/Users/casey/Library/Containers/com.apple.cloudphotosd/Data/Library/Application Support/com.apple.cloudphotosd/services/com.apple.photo.icloud.sharedstreams/assets/" --interval 20000
+./airphoto --firebase --db ~/Library/MediaStream/albumshare/1354123432+6916374443373062918/Model.sqlite --assets "~/Library/Containers/com.apple.cloudphotosd/Data/Library/Application Support/com.apple.cloudphotosd/services/com.apple.photo.icloud.sharedstreams/assets/" --interval 20000
