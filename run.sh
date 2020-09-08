c48306fcd298c73d372431e70ddbfb7fcb74a576
casey <casey@caseys-iMac-Pro.local>
2020-09-08 15:19:43 -0700

    change to zsh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-./airphoto-server --firebase --db ~/Library/MediaStream/albumshare/1354123432+6916374443373062918/Model.sqlite --assets "~/Library/Containers/com.apple.cloudphotosd/Data/Library/Application Support/com.apple.cloudphotosd/services/com.apple.photo.icloud.sharedstreams/assets/" --interval 20000
+#!/bin/zsh
+./airphoto-server --firebase --db ~/Library/MediaStream/albumshare/1354123432+6916374443373062918/Model.sqlite --assets "/Users/casey/Pictures/Photos Library.photoslibrary/resources/cloudsharing/data/1354123432/" --interval 20000
