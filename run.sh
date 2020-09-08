b874f93914d69bbd0b06d2187ebed0b4984ad417
casey <casey@caseys-iMac-Pro.local>
2020-09-08 14:47:39 -0700

    update build name

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./airphoto --firebase --db ~/Library/MediaStream/albumshare/1354123432+6916374443373062918/Model.sqlite --assets "~/Library/Containers/com.apple.cloudphotosd/Data/Library/Application Support/com.apple.cloudphotosd/services/com.apple.photo.icloud.sharedstreams/assets/" --interval 20000
+./airphoto-server --firebase --db ~/Library/MediaStream/albumshare/1354123432+6916374443373062918/Model.sqlite --assets "~/Library/Containers/com.apple.cloudphotosd/Data/Library/Application Support/com.apple.cloudphotosd/services/com.apple.photo.icloud.sharedstreams/assets/" --interval 20000
